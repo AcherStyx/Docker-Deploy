361f8f82eba8a64d81090372c8d880f17ac23497
AcherStyx <SYJ.Micro@outlook.com>
2020-06-23 20:12:42 +0800

    Add shell script to change docker mirror

---
+++ Shell-Script/change-mirror.sh
@@ -0,0 +1,8 @@
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
